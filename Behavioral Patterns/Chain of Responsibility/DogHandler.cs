6d8adb4aae2f6ba804e951aee40eb670d27b8ba3
David Asenov <david.asenoff@gmail.com>
2021-06-26 19:51:22 +0300

    Add implementation of a Chain of Responsibility

---
+++ Behavioral Patterns/Chain of Responsibility/DogHandler.cs
@@ -0,0 +1,17 @@
+﻿namespace Chain_of_Responsibility
+{
+    class DogHandler : AbstractHandler
+    {
+        public override object Handle(object request)
+        {
+            if (request.ToString() == "MeatBall")
+            {
+                return $"Dog: I'll eat the {request.ToString()}.\n";
+            }
+            else
+            {
+                return base.Handle(request);
+            }
+        }
+    }
+}
---
+++ Behavioral Patterns/Chain of Responsibility/MonkeyHandler.cs
@@ -0,0 +1,17 @@
+﻿namespace Chain_of_Responsibility
+{
+    class MonkeyHandler : AbstractHandler
+    {
+        public override object Handle(object request)
+        {
+            if ((request as string) == "Banana")
+            {
+                return $"Monkey: I'll eat the {request.ToString()}.\n";
+            }
+            else
+            {
+                return base.Handle(request);
+            }
+        }
+    }
+}
